ed0a3912c3d3908989c04e5bdd0e47a130c1846c
Ziddy9639 <didieriradukunda73@gmail.com>
2025-11-24 10:16:33 +0200

    Fix top_n function and update docstring

---
+++ README.md
@@ -0,0 +1,5 @@
+# mypackage
+This libary was created as an example of how to publish your own python package.
+
+# How to install 
+...
--- mypackage/myModule.py
+++ mypackage/myModule.py
@@ -1,21 +1,29 @@
 def top_n(items, n):
-    '''Return the top n items in array, in desc order.
+    """Return the top n items in an array, in descending order.
+
     Args:
-        items (array): list or array-like ibject
-        n (int): num of items to ruturn
-    Return:
-        array: top n items. in desc order
-    Egs:
-        >>> top_n([9,3,2,7,4],3)
-        [8,7,3]
-    '''
-for i in range (n): # keep sorting until we hace the top n items
-    for j in range(len(items)-1-i):
-        if items[j]>items[j+1]: # if this item is bigger than next item...
-            items[j], items[j+1]= items[j+1], items[j] # swao the two!
+        items (array): list or array-like object containing numerical values.
+        n (int): number of top items to return.
 
-# get last two items
-top_n= items[-n:] 
+    Returns:
+        array: top n items, in descending order.
 
-# return in descending order
-return top_n[::-1]
+    Examples:
+        >>> top_n([8, 3, 2, 7, 4], 3)
+        [8, 7, 4]
+    """
+    
+    # We add the body of the function just below the docstring:
+    
+    for i in range(n):  # Keep sorting until we have the top n items
+        for j in range(len(items)-1-i):
+
+            if items[j] > items[j+1]:  # If this item is bigger than next the item..
+                items[j], items[j+1] = items[j+1], items[j]  # swap the two!
+                
+    # Get last two items
+        top_n_items = items[-n:]
+    
+    # Return in descending order
+    return top_n_items[::-1]
+
